eef6af6418bc7bbba59bdc1f1e670a5e21b20565
phamlam13092k <pham.lam@lifesup.com.vn>
2024-06-16 22:29:38 +0700

    Update code login template

--- src/@core/libs/constants/routes.tsx
+++ src/@core/libs/constants/routes.tsx
@@ -1,10 +1,18 @@
 import { Route, createRoutesFromElements } from "react-router-dom";
 import RootLayout from "../layout/RootLayout";
 import { LoginLazyPage } from "./lazy";
+import { Suspense } from "react";
 
 const ROUTES = createRoutesFromElements(
   <Route path="/" element={<RootLayout />}>
-    <Route path="login" element={<LoginLazyPage />} />
+    <Route
+      path="login"
+      element={
+        <Suspense fallback={<p>Loading Login Page</p>}>
+          <LoginLazyPage />
+        </Suspense>
+      }
+    />
   </Route>
 );
 
--- src/modules/auth/templates/login.template.tsx
+++ src/modules/auth/templates/login.template.tsx
@@ -1,17 +1,33 @@
+import { Box, TextField, Typography } from "@mui/material";
 import useLoginController from "../controllers/login.controller";
 
 const LoginTemplate = () => {
   const { loginForm, handleSubmit } = useLoginController();
 
   return (
-    <div>
-      <h1>Login</h1>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <Typography variant="h1">Login</Typography>
       <form onSubmit={loginForm.handleSubmit(handleSubmit)}>
-        <input
+        {/* <input
           type="text"
           placeholder="Username"
           {...loginForm.register("username")}
+        /> */}
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          {...loginForm.register("username")}
         />
+
         <input
           type="password"
           placeholder="Password"
@@ -19,7 +35,7 @@ const LoginTemplate = () => {
         />
         <button type="submit">Login</button>
       </form>
-    </div>
+    </Box>
   );
 };
 
